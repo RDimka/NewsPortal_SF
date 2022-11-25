ba4d82b3cb7ef1091ed97ff0150795a7edd870cd
R.Dmitriy <rdimka@mail.ru>
2022-11-25 23:09:19 +0500

    1,2,3,4,5 release

--- NewsPaper/news_app/templatetags/custom_filters.py
+++ NewsPaper/news_app/templatetags/custom_filters.py
@@ -1,15 +1,31 @@
 from django import template
 
+class CensorException(Exception):
+   pass
+
 
 register = template.Library()
 
+#словарь
+CENSORED_WORDS = [
+   'spirit',
+   'palit',
+   'lovelace']
 
 # Регистрируем наш фильтр под именем,
 # что это именно фильтр для шаблонов, а не простая функция.
 @register.filter()
-def currency(value):
-   """
-   value: значение, к которому нужно применить фильтр
-   """
-   # Возвращаемое функцией значение подставится в шаблон.
-   return f'{value} Р'
+def censor(value):
+   try:
+      if not isinstance(value, str):
+         raise CensorException("Error: is not a text")
+
+      for word in CENSORED_WORDS:
+         if word in value:
+            value = value.replace(word, word[:1]+"***")
+         if word.capitalize() in value:
+            value = value.replace(word.capitalize(), word.capitalize()[:1]+"***")
+      # Возвращаемое функцией значение подставится в шаблон.
+      return value
+   except CensorException as e:
+      print (e)
