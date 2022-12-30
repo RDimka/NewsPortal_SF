6c5dca63bf267115708cd12edc8a1ec98404140c
R.Dmitriy <rdimka@mail.ru>
2022-12-31 02:02:40 +0500

    d8.4 allauth, login, logout

--- NewsPaper/sign/urls.py
+++ NewsPaper/sign/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from .views import BaseRegisterView
 from django.urls import include
 
 urlpatterns = [
@@ -10,4 +11,7 @@ urlpatterns = [
     path('logout/',
          LogoutView.as_view(template_name = 'sign/logout.html'),
          name='logout'),
+    path('signup/',
+         BaseRegisterView.as_view(template_name = 'sign/signup.html'),
+         name='signup'),
 ]
